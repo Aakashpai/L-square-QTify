b5b2fd9d722cf7009fbd40b6fed7494a98cdfceb
Aakashpai <aakash.pai@pebstone.com>
2023-11-11 11:56:49 +0530

    added header

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  .nav-wrapper{
+    background-color: #34c94b ;
+  }
+  nav{
+    background-color: #34c94b !important;
+    justify-content: space-between;
+    width: 95%;
+    margin: auto;
+  }
+  .feedback-btn{
+    background-color: #121212 !important;
+    color: #34c94b;
+  }
+  .form-control{
+    width: 25rem;
+    flex: none;
+    border-radius: 7px !important;
+  }
+  .input-group{
+    width: auto;
+  }
+  .input-group-append {
+    position: relative;
+    right: 42px;
+    top: 7px;
+    width: 42px;
+  }
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <div className="App">
+        <Header />
+      </div>
+    </Wrapper>
   );
 }
 
